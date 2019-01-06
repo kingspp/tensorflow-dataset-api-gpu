9bb23b5e3b1a959a1129d2ffbd43e996a7979e98
Prathyush SP <kingspprathyush@gmail.com>
2019-01-07 00:16:13 +0530

    updated example

--- experiments/EX002_Mnist_FFN_Variations/run.sh
+++ experiments/EX002_Mnist_FFN_Variations/run.sh
@@ -3,7 +3,7 @@
 export PYTHONPATH=$PYTHONPATH:/Users/prathyushsp/Git/tensorflow-dataset-api-gpu:/home/prathyush.sp/Git/tensorflow-dataset-api-gpu
 
 echo "##################################################################################################"
-echo "###           Running FFN Experiments                                                           ##"
+echo "###           Running FFN Experiments -  1                                                      ##"
 echo "##################################################################################################"
 
 
@@ -12,25 +12,46 @@ python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_1.json profi
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_1.json separate | tee mnist_ffn_config_gpu_1_1_separate.log
 python3.6 -u ./../../benchmark/report_generator.py ./mnist_ffn_config_gpu_1_1.json | tee mnist_ffn_config_gpu_1_1_report.log
 
+
+echo "##################################################################################################"
+echo "###           Running FFN Experiments -  2                                                      ##"
+echo "##################################################################################################"
+
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_2.json normal | tee mnist_ffn_config_gpu_1_2_normal.log
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_2.json profiled | tee mnist_ffn_config_gpu_1_2_profiled.log
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_2.json separate | tee mnist_ffn_config_gpu_1_2_separate.log
 python3.6 -u ./../../benchmark/report_generator.py ./mnist_ffn_config_gpu_1_2.json | tee mnist_ffn_config_gpu_1_2_report.log
+
+echo "##################################################################################################"
+echo "###           Running FFN Experiments -  3                                                      ##"
+echo "##################################################################################################"
 
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_3.json normal | tee mnist_ffn_config_gpu_1_3_normal.log
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_3.json profiled | tee mnist_ffn_config_gpu_1_3_profiled.log
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_3.json separate | tee mnist_ffn_config_gpu_1_3_separate.log
 python3.6 -u ./../../benchmark/report_generator.py ./mnist_ffn_config_gpu_1_3.json | tee mnist_ffn_config_gpu_1_3_report.log
 
+echo "##################################################################################################"
+echo "###           Running FFN Experiments -  4                                                      ##"
+echo "##################################################################################################"
+
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_4.json normal | tee mnist_ffn_config_gpu_1_4_normal.log
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_4.json profiled | tee mnist_ffn_config_gpu_1_4_profiled.log
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_4.json separate | tee mnist_ffn_config_gpu_1_4_separate.log
 python3.6 -u ./../../benchmark/report_generator.py ./mnist_ffn_config_gpu_1_4.json | tee mnist_ffn_config_gpu_1_4_report.log
 
+echo "##################################################################################################"
+echo "###           Running FFN Experiments -  5                                                      ##"
+echo "##################################################################################################"
+
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_5.json normal | tee mnist_ffn_config_gpu_1_5_normal.log
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_5.json profiled | tee mnist_ffn_config_gpu_1_5_profiled.log
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_5.json separate | tee mnist_ffn_config_gpu_1_5_separate.log
 python3.6 -u ./../../benchmark/report_generator.py ./mnist_ffn_config_gpu_1_5.json | tee mnist_ffn_config_gpu_1_5_report.log
+
+echo "##################################################################################################"
+echo "###           Running FFN Experiments -  6                                                      ##"
+echo "##################################################################################################"
 
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_6.json normal | tee mnist_ffn_config_gpu_1_6_normal.log
 python3.6 -u ./../../benchmark/template.py ./mnist_ffn_config_gpu_1_6.json profiled | tee mnist_ffn_config_gpu_1_6_profiled.log
